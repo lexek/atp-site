8b0eac99c30bf501c52e5b5b95117c31d1e5a815
Belostotskiy Alexey <lexek92@mail.ru>
2016-12-16 03:01:44 +0300

    change urls to https

--- static/channels/ChannelService.js
+++ static/channels/ChannelService.js
@@ -1,8 +1,8 @@
 (function () {
-    "use strict";
+    'use strict';
 
-    angular.module("channels")
-        .service("channelService", ["$q", "$http", ChannelService]);
+    angular.module('channels')
+        .service('channelService', ['$q', '$http', ChannelService]);
 
     function ChannelService($q, $http) {
         var firstFetch = true;
@@ -10,14 +10,14 @@
         return {
             fetchState: function() {
                 return $http({
-                    method: "GET",
-                    url: "http://atplay.ch:7777/new"
+                    method: 'GET',
+                    url: 'https://atplay.ch/backend/new'
                 }).then(function(response) {
                     if (response.status === 200) {
                         var data = response.data;
                         var status = data.channel.status;
-                        if (status.endsWith("@Play") && (status.indexOf("|") !== -1)) {
-                            var temp = status.substring(0, status.length - 5).split("|");
+                        if (status.endsWith('@Play') && (status.indexOf('|') !== -1)) {
+                            var temp = status.substring(0, status.length - 5).split('|');
                             data.channel.status = temp[0].trim();
                             data.channel.streamer = temp[1].trim();
                         }
@@ -27,13 +27,13 @@
                     } else {
                         $q.reject(response);
                     }
-                })
+                });
             },
             getInitialState: function () {
                 return {
                     update: false,
                     channel: {
-                        status: "ATPlay",
+                        status: 'ATPlay',
                         game: null,
                         streamer: null,
                         viewers: 0
@@ -42,29 +42,29 @@
                         {
                             id: 0,
                             online: false,
-                            channel: "atplay",
-                            provider: "cybergame",
+                            channel: 'atplay',
+                            provider: 'cybergame',
                             viewers: 0
                         },
                         {
                             id: 1,
                             online: false,
-                            channel: "atpchannel",
-                            provider: "twitch",
+                            channel: 'atpchannel',
+                            provider: 'twitch',
                             viewers: 0
                         },
                         {
                             id: 2,
                             online: false,
-                            channel: "38465",
-                            provider: "goodgame",
+                            channel: '38465',
+                            provider: 'goodgame',
                             viewers: 0
                         },
                         {
                             id: 3,
                             online: false,
-                            channel: "16970245",
-                            provider: "ustream",
+                            channel: '16970245',
+                            provider: 'ustream',
                             viewers: 0
                         }
                     ]
